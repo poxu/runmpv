4960f72dfe8571525754c1d5d3ecf47ac12d83d2
poxu <poxvuibr@gmail.com>
2021-02-04 20:33:26 +0300

    Create CompositeMpvRunnerProperties to separate logic for default values

---
+++ src/main/java/com/evilcorp/settings/CompositeMpvRunnerProperties.java
@@ -0,0 +1,69 @@
+package com.evilcorp.settings;
+
+import java.util.Properties;
+
+public class CompositeMpvRunnerProperties implements MpvRunnerProperties {
+    private final MpvRunnerProperties actualProperties;
+    private final MpvRunnerProperties defaultProperties;
+    private Properties properties;
+    private boolean propertiesLoaded;
+
+    public CompositeMpvRunnerProperties(
+            MpvRunnerProperties actualProperties,
+            MpvRunnerProperties defaultProperties
+    ) {
+
+        this.actualProperties = actualProperties;
+        this.defaultProperties = defaultProperties;
+    }
+
+    @Override
+    public Integer waitSeconds() {
+        final Integer waitSeconds = actualProperties.waitSeconds();
+        if (waitSeconds == null) {
+            return defaultProperties.waitSeconds();
+        }
+        return waitSeconds;
+    }
+
+    @Override
+    public String mpvHomeDir() {
+        final String mpvHomeDir = actualProperties.mpvHomeDir();
+        if (mpvHomeDir == null) {
+            return defaultProperties.mpvHomeDir();
+        }
+        return mpvHomeDir;
+    }
+
+    @Override
+    public String pipeName() {
+        final String pipeName = actualProperties.pipeName();
+        if (pipeName == null) {
+            return defaultProperties.pipeName();
+        }
+        return pipeName;
+    }
+
+    @Override
+    public String mpvLogFile() {
+        final String mpvLogFile = actualProperties.mpvLogFile();
+        if (mpvLogFile == null) {
+            return defaultProperties.mpvLogFile();
+        }
+        return mpvLogFile;
+    }
+
+    @Override
+    public String executableDir() {
+        return actualProperties.executableDir();
+    }
+
+    @Override
+    public String runnerLogFile() {
+        final String runnerLogFile = actualProperties.runnerLogFile();
+        if (runnerLogFile == null) {
+            return defaultProperties.runnerLogFile();
+        }
+        return runnerLogFile;
+    }
+}
