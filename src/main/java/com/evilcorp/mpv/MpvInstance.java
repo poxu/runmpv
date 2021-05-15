a899a598117e8c438c1fabab9ead74fa5346faed
poxu <poxvuibr@gmail.com>
2021-05-15 12:30:31 +0300

    add javadoc

--- src/main/java/com/evilcorp/mpv/MpvInstance.java
+++ src/main/java/com/evilcorp/mpv/MpvInstance.java
@@ -1,8 +1,23 @@
 package com.evilcorp.mpv;
 
+/**
+ * An interface for controlling an instance of mpv.
+ * Implementation should control one instance of mpv.
+ */
 public interface MpvInstance {
-
+    /**
+     * Send a command to mpv instance
+     * @param command action mpv should execute
+     */
     void execute(MpvCommand command);
 
+    /**
+     * Focus mpv window. It is not a command, because
+     * mpv doesn't have api to focus itself. It is supposed
+     * that you use window manager to do that.
+     * So, you either should have a different implementation
+     * of MpvInstance per window manager, or inject and implementation
+     * of focus via constructor.
+     */
     void focus();
 }
