837055d6b2b06a5aefcf0a9f69dabc4d37df56cd
poxu <poxvuibr@gmail.com>
2021-05-15 11:01:14 +0300

    fix imports

--- src/main/java/com/evilcorp/settings/TextFileSettings.java
+++ src/main/java/com/evilcorp/settings/TextFileSettings.java
@@ -1,6 +1,10 @@
 package com.evilcorp.settings;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Optional;
