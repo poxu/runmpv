1d64d8d82111891f32059d46a736ae54d610664c
poxu <poxvuibr@gmail.com>
2022-04-26 01:38:34 +0300

    Documentation for RunMpvProperties interface

--- src/main/java/com/evilcorp/settings/RunMpvProperties.java
+++ src/main/java/com/evilcorp/settings/RunMpvProperties.java
@@ -2,27 +2,32 @@ package com.evilcorp.settings;
 
 /**
  * Settings for runmpv.
+ *
+ * Unlike {@link SoftSettings}, this interface provides a separate
+ * method for each setting. This is done for programmer's convenience.
+ *
+ * Code completion works and all settings are typed.
  */
 public interface RunMpvProperties {
     /**
-     * Number of seconds to wait until mpv.exe starts,
+     * Number of seconds to wait until mpv executable starts,
      * before quitting with error.
      */
     Integer waitSeconds();
 
     /**
-     * Directory, where mpv.exe is located.
+     * Directory, where mpv executable is located.
      */
     String mpvHomeDir();
 
     /**
-     * Name of controlling pipe for mpv.exe
+     * Name of controlling pipe or unix domain socket for mpv
      */
     String pipeName();
 
     /**
-     * File where mpv.exe should write logs.
-     * Only mpv.exe, controlled by runmpv will write logs there.
+     * File where mpv executable should write logs.
+     * Only mpv executable, controlled by runmpv will write logs there.
      */
     String mpvLogFile();
 
@@ -32,17 +37,14 @@ public interface RunMpvProperties {
     String executableDir();
 
     /**
-     * File, where runmpv will put emergency logs,
-     * in case logging system fails.
-     * Regular logging system is controlled through,
-     * logging.properties
+     * File, where runmpv will put emergency logs, in case logging system fails.
+     * Regular logging system is controlled through logging.properties
      */
     String runnerLogFile();
 
     /**
-     * If true, mpv window is focused after new
-     * file is loaded. If false, mpv window
-     * is only focused on first launch
+     * If true, mpv window is focused after new file is loaded.
+     * If false, mpv window is only focused on first launch
      */
     boolean focusAfterOpen();
 }
