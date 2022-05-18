da2efb2638ada2339cb61e3bbf3c80189ccfab4a
poxu <poxvuibr@gmail.com>
2022-05-18 19:57:19 +0300

    Add synchronisation server implementation for testing and development purposes

---
+++ src/main/java/com/evilcorp/server/StartMpvServer.java
@@ -0,0 +1,62 @@
+package com.evilcorp.server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.Set;
+
+public class StartMpvServer {
+    public static void main(String[] args) throws IOException {
+        Selector selector = Selector.open();
+        final ServerSocketChannel server = ServerSocketChannel.open();
+        server.bind(new InetSocketAddress(34218));
+        server.configureBlocking(false);
+        final SelectionKey serverKey = server.register(selector, SelectionKey.OP_ACCEPT);
+        ByteBuffer buff = ByteBuffer.allocate(1000);
+
+        while (true) {
+            selector.select();
+            final Set<SelectionKey> keys = selector.selectedKeys();
+            for (Iterator<SelectionKey> iterator = keys.iterator(); iterator.hasNext(); ) {
+                SelectionKey key = iterator.next();
+                if (key.isAcceptable()) {
+                    final SocketChannel client = server.accept();
+                    client.configureBlocking(false);
+                    client.register(selector, SelectionKey.OP_READ);
+                }
+                if (key.isReadable()) {
+                    final SocketChannel client = (SocketChannel) key.channel();
+                    System.out.println("reading");
+                    final int readCode = client.read(buff);
+                    if (readCode == -1) {
+                        client.close();
+                        continue;
+                    }
+                    buff.flip();
+                    final String message = StandardCharsets.UTF_8.decode(buff).toString();
+                    buff.clear();
+                    System.out.println(message.length() + " '" + message + "'");
+                    client.write(StandardCharsets.UTF_8.encode("received\n"));
+                    for (SelectionKey selectionKey : selector.keys()) {
+                        if (selectionKey != key && selectionKey != serverKey) {
+                            try {
+                                ((SocketChannel) selectionKey.channel())
+                                        .write(StandardCharsets.UTF_8.encode(message));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                //message lost, but at least server didn't drop dead
+                            }
+                        }
+                    }
+                }
+                iterator.remove();
+            }
+        }
+    }
+}
